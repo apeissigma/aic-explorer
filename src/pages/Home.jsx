782a182d4f8114b447f786d3decc6b3493fd4689
imlipi <apeissigma@gmail.com>
2026-05-03 19:49:16 -0500

    Adds fonts, updates homepage styling, prep for daily works section

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,10 @@
 import '../css/App.css'
 import '../css/SearchBar.css'
+import '../css/Home.css'
 import ArtCard from '../components/ArtCard.jsx'
 import { useEffect, useState } from 'react'
 import { useNavigate } from 'react-router-dom'
-import { getArtworks, searchArtworks, getPopularArtworks } from "../services/api";
+import { getArtworks, searchArtworks, getPopularArtworks, getId } from "../services/api";
 
 function Home() {
   const [searchQuery, setSearchQuery] = useState("");
@@ -12,6 +13,25 @@ function Home() {
   const [error, setError] = useState(null);
   const [loading, setLoading] = useState(true);
   
+  /*
+  //load daily work
+  useEffect(() => {
+    const loadSingleWork = async () => {
+      try {
+        const artwork = await getId(16568);
+        setArtworks(artwork);
+      } catch (err) {
+        console.log(err);
+        setError(`${err}: Failed to load...`);
+      } finally {
+        setLoading(false);
+      }
+    };
+    loadSingleWork(); 
+  })
+    */
+
+
   //load popular art with getPopularArtworks on page load
   useEffect(() => {
     const loadArtworks = async () => {
@@ -50,13 +70,27 @@ function Home() {
         </form>
       </div>
 
-      <main>
-        <div className="hero">
+      <div className="hero">
+        <div className="hero-content">
           <h1>Explore the Art Institute of Chicago's Collection</h1>
         </div>
-        
-        
+      </div>
+
+      <main>
         {error && <div className="error-message">{error}</div>}
+
+        <div className="daily-work">
+          <h2>Daily Work</h2>
+          {loading ? (
+          <div className="loading">Loading...</div>
+          ) : (
+          <div className="artwork-grid">
+            {artworks.map((artwork) => (
+                <ArtCard key={artwork.id} artwork={artwork} />
+              ))}
+          </div>
+          )}
+        </div>
 
         <div className="popular-works">
           <h2>Selected Works</h2>
